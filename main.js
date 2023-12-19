bf04575cd0f0f84f3b8f106d4d2a471888f9245f
Maayan David <maayan500101@gmail.com>
2023-12-19 21:22:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function loadFunction() {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaXR5SWQiOjEwNywiam91cm5hbCI6ImFsbEhvc3BpdGFscyIsImlzQWRtaW4iOnRydWV9.2nG-_7H5mPS5DyEAZNE_lB4F5J-DGHC1V76q2txpz0g";
+  const srcUrl = "http://localhost:3000";
+
+  document.querySelector("iframe").contentWindow.postMessage(token, srcUrl);
+}
