2f97b9e3c4a596aefa361a039bf23f848ad890ef
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-20 10:40:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig,loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from "dotenv"
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_SHUAL_TOKEN': JSON.stringify(process.env.VITE_SHUAL_TOKEN || ''),
-    'process.env.VITE_NAME': JSON.stringify(process.env.VITE_NAME || ''),
     // Add other environment variables as needed
   },
 });
