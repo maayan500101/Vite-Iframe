e72f15d301b66c7ef0d9b916b3b5b440880a57a1
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-20 07:53:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function loadFunction() {
+  alert(process.env.REACT_APP_SHUAL_TOKEN)
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaXR5SWQiOjEwNywiam91cm5hbCI6ImFsbEhvc3BpdGFscyIsImlzQWRtaW4iOnRydWV9.2nG-_7H5mPS5DyEAZNE_lB4F5J-DGHC1V76q2txpz0g";
   const srcUrl = "http://localhost:3000";
