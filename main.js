69fd1ff681449094be3b3f0bea9013b324aa68f0
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-20 08:17:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 function loadFunction() {
-  alert(process.env.REACT_APP_SHUAL_TOKEN)
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaXR5SWQiOjEwNywiam91cm5hbCI6ImFsbEhvc3BpdGFscyIsImlzQWRtaW4iOnRydWV9.2nG-_7H5mPS5DyEAZNE_lB4F5J-DGHC1V76q2txpz0g";
   const srcUrl = "http://localhost:3000";
 
   document.querySelector("iframe").contentWindow.postMessage(token, srcUrl);
 }
-alert(process.env.REACT_APP_SHUAL_TOKEN)
+alert(import.meta.env.REACT_APP_SHUAL_TOKEN)
