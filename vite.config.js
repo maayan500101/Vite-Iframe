5ab724c1994ecbf0b4a276938b8c9c5afc3d105a
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-20 10:45:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from "dotenv"
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Add other environment variables as needed
-  },
 });
